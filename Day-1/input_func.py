b220afda28ee154907106add291358db82ae476b
Kalyan <kalyanchepena95@gmail.com>
2026-06-12 22:58:18 +0530

    Create input_func.py

---
+++ Day-1/input_func.py
@@ -0,0 +1,7 @@
+name = input("Enter your name : ")
+print("Hello " + name)
+
+age = input("Enter your age : ")
+print("Your age is " + age)
+
+print("Hello " + name + " from " + input("What is your native place? "))
